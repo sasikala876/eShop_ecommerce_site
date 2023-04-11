3059905b2af70121c75efdfbdb402483ea71f835
sasikala <2139749@cognizant.com>
2023-04-12 00:53:53 +0530

    fetching data

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+const cartDB = require('./models/CartPageSchema.js');
+const personalDB = require('./models/PersonalDetailsSchema.js');
+const addressDB = require('./models/AddressDetailsSchema.js');
+
 var cors = require('cors');
 var bodyParser = require('body-parser');
 
@@ -39,6 +43,43 @@ app.use(express.static(path.join(__dirname,"./frontend/build")));
 app.get("*", function(req,res) {
     res.sendFile(path.join(__dirname,"./frontend/build/index.html"));
 });
+
+app.get("/fetchall/Cart",(req,res)=>{
+    console.log("inside fetchAll");
+  cartDB.find((err,val)=>{
+        if(err){
+            console.log("err in getting the data");
+        }
+        else{
+            res.json(val);
+        }
+    })
+})
+
+app.get("/fetchall/personalData",(req,res)=>{
+    console.log("inside fetchAll");
+   personalDB.find((err,val)=>{
+        if(err){
+            console.log("err in getting the data");
+        }
+        else{
+            res.json(val);
+        }
+    })
+})
+
+app.get("/fetchall/addressData",(req,res)=>{
+    console.log("inside fetchAll");
+   addressDB.find((err,val)=>{
+        if(err){
+            console.log("err in getting the data");
+        }
+        else{
+            res.json(val);
+        }
+    })
+})
+
 
 app.listen(port,() => {
     console.log(`Express listening on port ${port}!`)
